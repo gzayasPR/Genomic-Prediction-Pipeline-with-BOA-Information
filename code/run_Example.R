5f0d5ef33edfaa9fc80bf230f7ecc96a42548359
Gabriel Zayas <124057156+gzayasPR@users.noreply.github.com>
2024-04-21 13:49:23 -0400

    Update run_Example.R
    Added decompression of SNP.raw.gz and BOA.raw.gz for running the example

--- code/run_Example.R
+++ code/run_Example.R
@@ -17,5 +17,25 @@ file.copy("3.Matrix_Structure_parameters.R",
 file.copy("4.Running_Models_parameters.R", 
           paste(results_dir,"/4.Running_Models/input/4.Running_Models_parameters.R",sep=""), 
           overwrite = TRUE)
+setwd(paste(data_dir,"/Example/",sep=""))
 
+
+# Define the paths to your .gz files
+gz_file1 <- "BOA.raw.gz"
+gz_file2 <- "SNP.raw.gz"
+
+# Specify the directory where you want to extract the contents
+extract_to_directory <- paste(data_dir,"/Example/",sep="")
+
+# Unzip the first .gz file
+gz1 <- gzfile(gz_file1, "rb")
+unzipped_file1 <- paste0(extract_to_directory, "BOA.raw")  # Adjust the output file name as needed
+writeLines(readLines(gz1), unzipped_file1)
+close(gz1)
+
+# Unzip the second .gz file
+gz2 <- gzfile(gz_file2, "rb")
+unzipped_file2 <- paste0(extract_to_directory, "SNP.raw")  # Adjust the output file name as needed
+writeLines(readLines(gz2), unzipped_file2)
+close(gz2)
 setwd(code_dir)
